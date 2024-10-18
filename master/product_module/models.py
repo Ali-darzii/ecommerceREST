806ce103bef602dd4c9820bec5da0414adbe7e2c
ali <ali.darzi.1354@gmail.com>
2024-10-18 17:30:41 +0330

    product visit provided !

--- master/product_module/models.py
+++ master/product_module/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from auth_module.models import User
+
 
 class ProductCategory(models.Model):
     title = models.CharField(max_length=300, db_index=True)
@@ -63,3 +65,16 @@ class ProductGallery(models.Model):
         verbose_name = 'Product Gallery'
         verbose_name_plural = 'Product Galleries'
         db_table = "Galleries_DB"
+
+
+class ProductVisit(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    ip = models.CharField(max_length=30)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product.title} | {self.ip}"
+
+    class Meta:
+        verbose_name = 'Product Visit'
+        verbose_name_plural = 'Product Visits'
--- master/product_module/views.py
+++ master/product_module/views.py
@@ -4,7 +4,12 @@ from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet
 from rest_framework.mixins import RetrieveModelMixin
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+
+from product_module.tasks import product_visited
 from utils.filters import ProductFilter
+from rest_framework.response import Response
+
+from utils.utils import get_client_ip
 
 
 class ProductViewSet(ReadOnlyModelViewSet):
@@ -14,6 +19,12 @@ class ProductViewSet(ReadOnlyModelViewSet):
     filterset_class = ProductFilter
     search_fields = ['name', 'description']
     ordering_fields = ['price']
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        product_visited.apply_async(args=(instance, get_client_ip(request), request.user.id))
+        return Response(serializer.data)
 
 
 class ProductCategoryViewSet(ReadOnlyModelViewSet):
